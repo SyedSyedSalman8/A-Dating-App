8dee93ca75a1ca4c55ff8ecde1be7183ac905b8d
Syed Salman Manzoor <immasalman@gmail.com>
2020-03-31 22:00:06 +0500

    end of extending api

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Models;
+using DatingApp.API.DTO;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+using AutoMapper;
+
+namespace DatingApp.API.Controllers
+{   
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {   
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+        public UsersController(IDatingRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsers(){
+            var users = await _repo.GetUsers();
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDTO>>(users);
+
+            return Ok(usersToReturn);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id){
+            var user = await _repo.GetUser(id);
+            var userToReturn = _mapper.Map<UserForDetailedDto>(user);
+            return Ok(userToReturn);
+        }
+
+    }
+}
